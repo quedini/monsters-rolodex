1469771d426d1ef483eff4f3c5be86f267e91662
Joel Lester <jflester@gmail.com>
2020-04-03 22:48:02 -0500

    Updates

---
+++ precache-manifest.995150f5a69ed99de4406e0a181843f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9121351319be23f83e1670ef911289d0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6275add338b58f5d04e1",
+    "url": "/monsters-rolodex/static/css/main.bd8ccec6.chunk.css"
+  },
+  {
+    "revision": "5d316147a2e5fc5731b1",
+    "url": "/monsters-rolodex/static/js/2.e67c12c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e67c12c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6275add338b58f5d04e1",
+    "url": "/monsters-rolodex/static/js/main.9782704d.chunk.js"
+  },
+  {
+    "revision": "582b45757d30545d66b4",
+    "url": "/monsters-rolodex/static/js/runtime-main.043f756d.js"
+  }
+]);
